6d94b58d29275ab1b276c6fd5ed81be56b91f3ff
Eyob <eyobmalik@gmail.com>
2024-03-18 00:03:20 +0300

    epoll usage with multiple TCP streams

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ const express = require("express");
 const app = express();
 
 app.get("/:delay/:message", (req, res) => {
-  const { delay, message } = req.params;
-  setTimeout(() => res.send(message), delay);
+    const { delay, message } = req.params;
+    console.log(
+        `Request received with delay: ${delay / 1000} seconds and message: ${message}`,
+    );
+    setTimeout(() => res.send(message), delay);
 });
 
 app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+    console.log("Server is running on port 8000");
 });
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use std::{
     ffi::{c_int, c_long, c_uint},
-    io::{Read, Write},
+    io::{self, BufReader, Read, Write},
     os::fd::AsRawFd,
     process::exit,
 };
 
 #[link(name = "c")]
 extern "C" {
-    pub fn epoll_create1(fd: c_int) -> c_int;
-    pub fn epoll_ctl(epfd: c_int, op: c_int, fd: c_int, event: *mut Event) -> c_int;
-    pub fn epoll_wait(epfd: c_int, events: *mut Event, max_events: c_int, timeout: c_int) -> c_int;
+    fn epoll_create1(fd: c_int) -> c_int;
+    fn epoll_ctl(epfd: c_int, op: c_int, fd: c_int, event: *mut Event) -> c_int;
+    fn epoll_wait(epfd: c_int, events: *mut Event, max_events: c_int, timeout: c_int) -> c_int;
+    fn close(epfd: c_int) -> c_int;
+
 }
 
 #[derive(Debug)]
@@ -35,53 +37,81 @@ fn get_req(path: &str) -> String {
 fn main() {
     let epoll_instance = unsafe { epoll_create1(0) };
     if epoll_instance == -1 {
-        println!("Error happened");
+        println!("Error occurred creating epoll instance");
         exit(-1);
     }
+    let mut streams = Vec::new();
 
-    let request = get_req("/5000/request-5");
-    let mut stream = std::net::TcpStream::connect("localhost:8080").unwrap();
-    stream.set_nonblocking(true).unwrap();
+    for i in 0..5 {
+        let delay = i * 1000;
+        let url = format!("/{delay}/{i}-Req");
+        let request = get_req(&url);
+        let mut stream = std::net::TcpStream::connect("localhost:8000").unwrap();
+        stream.set_nonblocking(true).unwrap();
 
-    stream.write_all(request.as_bytes()).unwrap();
+        stream.write_all(request.as_bytes()).unwrap();
 
-    let res = unsafe {
-        let mut event = Event {
-            events: (EPOLLIN | EPOLLET) as u32,
-            data: 5,
+        let res = unsafe {
+            let mut event = Event {
+                events: (EPOLLIN | EPOLLET) as u32,
+                data: i,
+            };
+            epoll_ctl(
+                epoll_instance,
+                EPOLL_CTL_ADD,
+                stream.as_raw_fd(),
+                &mut event,
+            )
         };
-        epoll_ctl(
-            epoll_instance,
-            EPOLL_CTL_ADD,
-            stream.as_raw_fd(),
-            &mut event,
-        )
-    };
-    if res == -1 {
-        println!("Error occurred registering stream in epoll");
-        exit(-1);
-    }
-
-    let mut data = [0u8; 4096];
-    let mut events: Vec<Event> = Vec::with_capacity(1);
-    unsafe {
-        let res = epoll_wait(epoll_instance, events.as_mut_ptr(), 1, -1);
-        if res == 0 {
-            println!("False alarm");
+        if res == -1 {
+            println!("Error occurred registering stream in epoll");
             exit(-1);
         }
+        streams.push(stream);
+    }
 
-        events.set_len(res as usize);
-    };
+    let mut handled = 0;
 
-    match stream.read(&mut data) {
-        Ok(n) => {
-            let txt = String::from_utf8_lossy(&data[..n]);
-            println!("{txt}");
+    while handled < 5 {
+        let mut events: Vec<Event> = Vec::with_capacity(1);
+        unsafe {
+            let res = epoll_wait(epoll_instance, events.as_mut_ptr(), 1, -1);
+            if res == 0 {
+                println!("False alarm");
+                exit(-1);
+            }
+
+            events.set_len(res as usize);
+        };
+
+        for event in events {
+            let index: usize = event.data as usize;
+            let mut data = [0u8; 64];
+            let mut reader = BufReader::new(&mut streams[index]);
+            loop {
+                match reader.read(&mut data) {
+                    Ok(0) => {
+                        handled += 1;
+                        break;
+                    }
+                    Ok(n) => {
+                        let txt = String::from_utf8_lossy(&data[..n]);
+                        println!("{txt}");
+                    }
+                    Err(e) if e.kind() == io::ErrorKind::WouldBlock => break,
+                    Err(e) => {
+                        println!("Error occurred");
+                        println!("{}", e);
+                        break;
+                    }
+                }
+            }
         }
-        Err(e) => {
-            println!("Error happened");
-            println!("{}", e);
-        }
+    }
+
+    let res = unsafe { close(epoll_instance) };
+    if res == -1 {
+        println!("Error closing epoll instance");
+        exit(-1)
     }
 }
